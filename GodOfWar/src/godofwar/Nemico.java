9ff2d49c760c640128aef972534b068039d45144
riccidavide16 <ricci.davide@franchettisalviani.net>
2026-03-07 11:54:26 +0100

    creazioni classi dopo UML

---
+++ GodOfWar/src/godofwar/Nemico.java
@@ -0,0 +1,36 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package godofwar;
+
+/**
+ *
+ * @author Utente
+ */
+public class Nemico {
+    
+    private String nome;
+    private int vita;
+    private int attacco;
+    
+    public Nemico(String nome,int vita,int attacco)
+    {
+        this.nome = nome;
+        this.vita = vita;
+        this.attacco = attacco;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public int getVita() {
+        return vita;
+    }
+
+    public int getAttacco() {
+        return attacco;
+    }
+    
+}
